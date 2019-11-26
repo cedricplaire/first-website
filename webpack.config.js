1f9d2eb81dfac8f5cfa6db63b8cc3421ad202fb6
CédricPlaire <againmusician@gmail.com>
2019-11-26 14:50:11 +0100

    commit sur master

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ Encore
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    .setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
@@ -24,8 +24,10 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('login', './assets/js/login.js')
+    .addEntry('admin', './assets/js/admin.js')
+    .addEntry('search', './assets/js/search.js')
+
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
