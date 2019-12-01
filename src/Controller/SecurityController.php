c449c9f4f8565ca902ce982ca6d0bc7b2f88357b
CédricPlaire <againmusician@gmail.com>
2019-12-01 04:38:33 +0100

    commit sur master

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,47 +10,34 @@ use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-/**
- * @author Plaire Cédric <plaire.cedric@somusicshare.com>
- */
 
 class SecurityController extends AbstractController
 {
     use TargetPathTrait;
-
     /**
      * @Route("/login", name="security_login")
      */
-    public function login(Request $request, Security $security, AuthenticationUtils $helper): Response
+    public function login(Request $request, Security $security, AuthenticationUtils $authenticationUtils): Response
     {
-        // if user is already logged in, don't display the login page again
-        if ($security->isGranted('ROLE_USER')) {
+        if ($this->getUser()) {
             return $this->redirectToRoute('blog_index');
         }
-        // this statement solves an edge-case: if you change the locale in the login
-        // page, after a successful login you are redirected to a page in the previous
-        // locale. This code regenerates the referrer URL whenever the login page is
-        // browsed, to ensure that its locale is always the current one.
-        $this->saveTargetPath($request->getSession(), 'main', $this->generateUrl('blog_index'));
+        
+        $this->saveTargetPath($request->getSession(), 'main', $this->generateUrl('admin_index'));
 
-        return $this->render('security/login.html.twig', [
-            // last username entered by the user (if any)
-            'last_username' => $helper->getLastUsername(),
-            // last authentication error (if any)
-            'error' => $helper->getLastAuthenticationError(),
-        ]);
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
-     * This is the route the user can use to logout.
-     *
-     * But, this will never be executed. Symfony will intercept this first
-     * and handle the logout automatically. See logout in config/packages/security.yaml
-     *
      * @Route("/logout", name="security_logout")
      */
-    public function logout(): void
+    public function logout()
     {
-        throw new \Exception('This should never be reached!');
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
